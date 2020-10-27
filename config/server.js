588b91f4f8f26ccf25c251b01ade0814ae4a4969
mhodges <michaelcshodges@gmail.com>
2020-10-27 22:07:15 +1300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6eeed478e355577455dd1ef6e2e6888e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '47b116b4-69ea-4f4a-bc4a-c873d201ca17'
+};
