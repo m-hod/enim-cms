a0b1bb800eb2178e0002d0805c867cde24e4b5ee
michael <michaelcshodges@gmail.com>
2020-12-05 10:58:46 +0000

    update server config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://admin.m-hodges.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6eeed478e355577455dd1ef6e2e6888e'),
