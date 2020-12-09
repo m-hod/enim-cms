8a69f1ca2b3e60fd0076773cfa3d5c6be84f57fc
m-hodges <michaelcshodges@gmail.com>
2020-12-09 17:03:42 -0500

    update content models

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: 'https://admin.m-hodges.com',
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://admin.m-hodges.com",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
