0b9e9bd7883472d954413d094b8e03f6ca184346
m-hodges <50115036+m-hodges@users.noreply.github.com>
2021-03-17 23:17:23 +1300

    update server file to consume .env for domain

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://admin.m-hodges.com",
+  url: process.env.DOMAIN,
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET"),
+      secret: env("ADMIN_JWT_SECRET", process.env.ADMIN_JWT_SECRET),
     },
   },
 });
